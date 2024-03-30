8d28ab3d4e2d4a761a10f291c4564450301954e1
anonfedora <musaeleazar090@gmail.com>
2024-03-30 17:44:14 +0100

    isAuthenticated fix01

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import  { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
+import  { authorizeRoles } from "../middlewares/auth.js";
 import { signUp, login, getAllUsers, getUser, forgotPassword, resetPassword } from "../controllers/userController.js";
 
 //TODO - import { requireSignIn } from "../helpers/authHelpers.js";
@@ -9,7 +9,7 @@ const router = Router();
 router.post("/signup", signUp);
 router.post("/login", login);
 router.post("/users", getAllUsers);
-router.get("/:id", isAuthenticatedUser, getUser);
+router.get("/:id", getUser);
 router.post("/forgot-password", forgotPassword);
 router.patch("/reset-password/:token", resetPassword);
 
