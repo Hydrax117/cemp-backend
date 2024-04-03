d8d4cbb2521f3fbd09cb5289a1d993ecfb06e921
anonfedora <musaeleazar090@gmail.com>
2024-04-03 13:45:10 +0100

    implement sendToken 00

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,13 +7,13 @@ const sendToken = async (user, statusCode, res) => {
     console.log(token);
     // options for cookie
     const options = {
-        expires: new Date(
+        expiresIn: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true
     };
 
-    res.status(statusCode).cookie("token", token, options).json({
+    res.status(statusCode).cookie("token", token).json({
         success: true,
         user,
         token
