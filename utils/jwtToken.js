b5f5432171e8809718bfb13342172c5bdcc69cbd
anonfedora <musaeleazar090@gmail.com>
2024-04-11 10:23:32 +0100

    jwtToken

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = async (user, statusCode, res) => {
     console.log(token);
     console.log("sendToken generateToken(*,*) above");
 
-    res.status(statusCode).cookie("token", token, { secure: false }).json({
+    res.status(statusCode).cookie(token, { secure: false }).json({
         success: true,
         data: {
             user
