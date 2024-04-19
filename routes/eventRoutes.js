76adfc85fa4bfe29893c63562a774b81d22ed9f9
hydrax117 <pauljoseph5000@gmail.com>
2024-04-19 23:38:28 +0100

    added middlewares

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 const router = Router();
 import { createNewEvent,updateEvent,deleteEvent,getOneEvent,searchEvent,eventRegistration,getAllEvents,registeredUsers,eventUnRegister } from "../controllers/eventController.js";
+import { isAuthenticatedUser } from "../middlewares/auth.js";
 router.post("/create", createNewEvent);
 router.get("/search", searchEvent);
 router.get("/get-one-event", getOneEvent);
@@ -9,6 +10,6 @@ router.get("/:id/registered",registeredUsers)
 router.put("/update/:id", updateEvent);
 router.delete("/delete/:id", deleteEvent);
 router.post("/:id/register",eventRegistration)
-router.delete('/:id/unregister',eventUnRegister)
+router.delete('/:id/unregister',isAuthenticatedUser,eventUnRegister)
 
 export default router;
